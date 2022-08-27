e8a9682f08f7ea5d951c7c7186d058aead87549b
johnworker <78348941+johnworker@users.noreply.github.com>
2022-08-27 10:51:50 +0800

    調整腳本

--- Assets/Scripts/ThirdPersonShooterController.cs
+++ Assets/Scripts/ThirdPersonShooterController.cs
@@ -1,3 +1,4 @@
+using StarterAssets;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,15 +6,24 @@ using UnityEngine;
 public class ThirdPersonShooterController : MonoBehaviour
 {
     public GameObject shootCamGameObj;
+
+    private StarterAssetsInputs _sai;
     // Start is called before the first frame update
     void Start()
     {
-        
+        this._sai = this.GetComponent<StarterAssetsInputs>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (this._sai.aim)
+        {
+            this.shootCamGameObj.SetActive(true);
+        }
+        else
+        {
+            this.shootCamGameObj.SetActive(false);
+        }
     }
 }
