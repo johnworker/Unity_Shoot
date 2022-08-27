e6c4ffd17132d8a2a64ddb2eb6ce3d71c031f187
johnworker <78348941+johnworker@users.noreply.github.com>
2022-08-27 10:25:45 +0800

    添加腳本射擊同步鏡頭移動

---
+++ Assets/Scripts/ThirdPersonShooterController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonShooterController : MonoBehaviour
+{
+    public GameObject shootCamGameObj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
